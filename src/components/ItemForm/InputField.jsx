7befa556e5a8a031054914f5074f46867ecafa69
Emiliano Gusberti <emggw@hotmail.com>
2023-06-04 20:46:42 -0300

    Add inputFieldId var to inputs

--- src/components/ItemForm/InputField.jsx
+++ src/components/ItemForm/InputField.jsx
@@ -3,17 +3,22 @@ import styles from "./styles.module.css";
 
 export default function InputField(props) {
   const { name, value, type, onChange, textContent = "", autoComplete = "off" } = props;
+  const inputFieldId = `input-form-${name}`;
 
   return (
     <div className={styles.inputContainer}>
-      <input className={styles.inputField}
+      <input
+        className={styles.inputField}
+        id={inputFieldId}
         name={name}
         type={type}
         onChange={onChange}
         value={value}
         autoComplete={autoComplete}
       />
-      <label className={styles.inputLabel} htmlFor={name}>{textContent}</label>
+      <label className={styles.inputLabel} htmlFor={inputFieldId}>
+        {textContent}
+      </label>
     </div>
   );
 }
