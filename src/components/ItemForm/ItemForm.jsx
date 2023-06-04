5dd900a0f0207194b4933153198c199ea89b3413
Emiliano Gusberti <emggw@hotmail.com>
2023-06-04 16:18:02 -0300

    Change labels

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -22,16 +22,16 @@ export default function ItemForm() {
     <div className={styles.container}>
       <h1 className={styles.title}>Cargar Producto</h1>
       <form className={styles.inputForm}>
-        <InputField name="name" value={product.name} type="text" textContent="Nombre del Producto" onChange={handleChange} />
+        <InputField name="name" value={product.name} type="text" textContent="Nombre" onChange={handleChange} />
         <InputField
           name="description"
           value={product.description}
           type="text"
-          textContent="Descripción del Producto (opcional)"
+          textContent="Descripción (opcional)"
           onChange={handleChange}
         />
-        <InputField name="price" value={product.price} type="number" textContent="Precio del Producto" onChange={handleChange} />
-        <button className="form-button" onClick={handleClick}>Agregar</button>
+        <InputField name="price" value={product.price} type="number" textContent="Precio" onChange={handleChange} />
+        <button className={styles.formButton} onClick={handleClick}>Agregar</button>
       </form>
     </div>
   );
