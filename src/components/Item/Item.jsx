43a68a370a61227e6ee784da5c3686508cb0790a
Emiliano Gusberti <emggw@hotmail.com>
2023-06-04 20:51:48 -0300

    Put button inside container

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,16 +1,21 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import styles from "./styles.module.css";
 
 export default function Item(props) {
+  const { itemCount, setItemCount } = useState(0);
   const { name, description, price, id, onDelete } = props;
 
   return (
     <div id={`item${id}`} className={styles.item}>
       <h1>{name}</h1>
       <h2>{description}</h2>
-      <h3>Precio: $ {price}</h3>
-      <p>id: {id}</p>
-      <button onClick={() => onDelete(id)}>Borrar</button>
+      <h3>$ {price}</h3>
+      <div className={styles.buttonContainer}>
+        <button className={styles.button} onClick={() => onDelete(id)}>
+          Borrar
+        </button>
+      </div>
     </div>
   );
 }
