108b8416221b77df3c2a5f08883e5c960cf6ff19
Emiliano Gusberti <emggw@hotmail.com>
2023-06-04 11:30:14 -0300

    Create product object, fix inputs, style

--- src/components/ItemForm/InputField.jsx
+++ src/components/ItemForm/InputField.jsx
@@ -1,22 +1,17 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
 import styles from "./styles.module.css";
 
 export default function InputField(props) {
-  const { name, type, defaultValue, textContent = "", required = true, autoComplete = "off" } = props;
-  const [value, setValue] = useState(defaultValue);
+  const { name, value, type, onChange, textContent = "", autoComplete = "off" } = props;
 
   return (
     <div className={styles.inputContainer}>
       <input className={styles.inputField}
         name={name}
         type={type}
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
+        onChange={onChange}
         value={value}
         autoComplete={autoComplete}
-        {...required && "required"}		
       />
       <label className={styles.inputLabel} htmlFor={name}>{textContent}</label>
     </div>
--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -1,19 +1,37 @@
+import { useState } from "react";
 import styles from "./styles.module.css";
 import InputField from "./InputField";
 
 export default function ItemForm() {
+  const [product, setProduct] = useState({ name: "", description: "", price: "" });
+
   function handleClick(event) {
     event.preventDefault();
+    console.log(product);
+    setProduct({ name: "", description: "", price: "" });
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setProduct((prevProduct) => {
+      return { ...prevProduct, [name]: value };
+    });
   }
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Cargar Producto</h1>
       <form className={styles.inputForm}>
-        <InputField name="name" type="text" textContent="Nombre del Producto" />
-        <InputField name="description" type="text" textContent="Descripción del Producto (opcional)" required={false} />
-        <InputField name="price" type="number" defaultValue={0} textContent="Precio del Producto" />
-        <button onClick={handleClick}>Agregar</button>
+        <InputField name="name" value={product.name} type="text" textContent="Nombre del Producto" onChange={handleChange} />
+        <InputField
+          name="description"
+          value={product.description}
+          type="text"
+          textContent="Descripción del Producto (opcional)"
+          onChange={handleChange}
+        />
+        <InputField name="price" value={product.price} type="number" textContent="Precio del Producto" onChange={handleChange} />
+        <button className="form-button" onClick={handleClick}>Agregar</button>
       </form>
     </div>
   );
