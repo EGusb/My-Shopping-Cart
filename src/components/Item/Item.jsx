61521950799616f89e559ae75aadca4e1e1076b0
Emiliano Gusberti <emggw@hotmail.com>
2023-06-04 22:11:20 -0300

    Add logic to item count sum & substraction

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,25 +3,37 @@ import { useState } from "react";
 import styles from "./styles.module.css";
 
 export default function Item(props) {
-  const [itemCount, setItemCount] = useState(0);
   const { name, description, price, id, onDelete } = props;
+  const [itemCount, setItemCount] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(Number(0).toFixed(2));
+
+  function handleClick(num) {
+    setItemCount((prevItemCount) => {
+      if (prevItemCount > 0 || (prevItemCount === 0 && num > 0)) {
+        prevItemCount += num;
+        setTotalPrice(Number(price * prevItemCount).toFixed(2));
+      }
+      return prevItemCount;
+    });
+  }
 
   return (
     <div id={`item${id}`} className={styles.item}>
       <h1>{name}</h1>
       <h2>{description}</h2>
-      <h3>$ {price}</h3>
+      <h4>$ {price}</h4>
       <div className={styles.buttonContainer}>
         {/* <button className={styles.button} onClick={() => onDelete(id)}>
           <i className="fa-solid fa-trash-can"></i>
         </button> */}
-        <button className={styles.button} onClick={() => setItemCount(itemCount - 1)}>
+        <button className={styles.button} onClick={() => handleClick(-1)}>
           -
         </button>
         <span className={styles.itemCount}>{itemCount}</span>
-        <button className={styles.button} onClick={() => setItemCount(itemCount + 1)}>
+        <button className={styles.button} onClick={() => handleClick(1)}>
           +
         </button>
+        <h3>{totalPrice}</h3>
       </div>
     </div>
   );
