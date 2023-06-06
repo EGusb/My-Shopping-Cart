2be25b693acc3f828d8fe273a015d94f71ef50df
Emiliano Gusberti <emggw@hotmail.com>
2023-06-06 19:16:35 -0300

    Add cart icon, containers, style

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -17,8 +17,13 @@ export default function ItemList(props) {
 
   return (
     <div className={styles.listContainer}>
-      <h1 className={styles.title}>Listado</h1>
-      <h2 className={styles.title}>Total carrito: $ {cartTotal}</h2>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>Listado</h1>
+        <div className={styles.cartContainer}>
+          <i className={`fa-solid fa-cart-shopping ${styles.cartIcon}`}></i>
+          <h3 className={styles.cartTotal}> $ {cartTotal}</h3>
+        </div>
+      </div>
       <div className={styles.list}>
         {children.map((item) => {
           return (
