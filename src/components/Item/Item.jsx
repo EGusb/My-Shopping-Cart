6b9bd1847f3d8804920dde03a1e845faae25cc6c
Emiliano Gusberti <emggw@hotmail.com>
2023-06-05 23:45:26 -0300

    Add cart total value, fix add & remove one item

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,18 +3,30 @@ import { useState } from "react";
 import styles from "./styles.module.css";
 
 export default function Item(props) {
-  const { name, description, price, id, onDelete: deleteItem } = props;
+  const { name, description, price, id, addToCart, fixNum, onDelete: deleteItem } = props;
   const [itemCount, setItemCount] = useState(0);
-  const [totalPrice, setTotalPrice] = useState(Number(0).toFixed(2));
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  function handleClick(num) {
-    setItemCount((prevItemCount) => {
-      if (prevItemCount > 0 || (prevItemCount === 0 && num > 0)) {
-        prevItemCount += num;
-        setTotalPrice(Number(price * prevItemCount).toFixed(2));
-      }
-      return prevItemCount;
-    });
+  function addOneItem() {
+    setItemCount((prevItemCount) => prevItemCount + 1);
+    setTotalPrice((prevTotalPrice) => fixNum(prevTotalPrice + price));
+    addToCart(fixNum(price));
+  }
+
+  function removeOneItem() {
+    setItemCount((prevItemCount) => Math.max(prevItemCount - 1, 0));
+    setTotalPrice((prevTotalPrice) => fixNum(Math.max(prevTotalPrice - price, 0)));
+
+    if (itemCount > 0) {
+      addToCart(fixNum(-price));
+    }
+  }
+
+  function handleDeleteItem() {
+    for (let i = 0; i < itemCount; i++) {
+      removeOneItem();
+    }
+    deleteItem(id);
   }
 
   return (
@@ -23,15 +35,15 @@ export default function Item(props) {
       <p>{description}</p>
       <h4>$ {price}</h4>
       <div className={styles.buttonContainer}>
-        <button className={styles.button} onClick={() => handleClick(-1)}>
+        <button className={styles.button} onClick={removeOneItem}>
           -
         </button>
         <span className={styles.itemCount}>{itemCount}</span>
-        <button className={styles.button} onClick={() => handleClick(1)}>
+        <button className={styles.button} onClick={addOneItem}>
           +
         </button>
         <h3>$ {totalPrice}</h3>
-        <button className={styles.buttonDelete} onClick={() => deleteItem(id)}>
+        <button className={styles.buttonDelete} onClick={handleDeleteItem}>
           <i className="fa-solid fa-trash-can"></i>
         </button>
       </div>
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import styles from "./styles.module.css";
 import Item from "../Item/Item";
 
 export default function ItemList(props) {
   const { children, onDelete } = props;
+  const [cartTotal, setCartTotal] = useState(0);
+
+  function fixNum(num) {
+    return Number((Math.round(num * 100) / 100).toFixed(2));
+  }
+
+  function addToCart(value) {
+    setCartTotal((prevCartTotal) => fixNum(Math.max(prevCartTotal + value, 0)));
+  }
 
   return (
     <div className={styles.listContainer}>
       <h1 className={styles.title}>Listado</h1>
+      <h2 className={styles.title}>Total carrito: $ {cartTotal}</h2>
       <div className={styles.list}>
         {children.map((item) => {
           return (
@@ -18,6 +29,8 @@ export default function ItemList(props) {
               id={item.id}
               key={item.id}
               onDelete={onDelete}
+              addToCart={addToCart}
+              fixNum={fixNum}
             />
           );
         })}
